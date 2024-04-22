7b2031511ba851afe099195554fb3d333d58a53d
Quentin Vignan <quentin.vignan@gmail.com>
2024-04-23 00:15:48 +0200

    Updates

---
+++ precache-manifest.fd77229d63440113357d306052299c59.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec90076d02eceb7d7c5a3f9d0f9e2818",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "98f2adfa5648e30eedb9",
+    "url": "/my-portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "fab91655f61011e614eb",
+    "url": "/my-portfolio/static/css/main.64388ba0.chunk.css"
+  },
+  {
+    "revision": "98f2adfa5648e30eedb9",
+    "url": "/my-portfolio/static/js/2.9846fbcf.chunk.js"
+  },
+  {
+    "revision": "923a93445da4455d8e7e236ec4fa5442",
+    "url": "/my-portfolio/static/js/2.9846fbcf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fab91655f61011e614eb",
+    "url": "/my-portfolio/static/js/main.c5815afd.chunk.js"
+  },
+  {
+    "revision": "28d97e6c4183a7c87d51",
+    "url": "/my-portfolio/static/js/runtime-main.9994cf37.js"
+  }
+]);
